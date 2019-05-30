3cfe3e67a23bc3db88662f47e10d61bd19d8f576
jaydeepchakraborty <jaydeep.chakraborty.1988@gmail.com>
2019-05-30 23:11:29 +0530

    Added Java model/pojo classes for entities

---
+++ linking_java/OntoSimilarity/src/main/java/com/ontosim/model/OntoReferenceModel.java
@@ -0,0 +1,50 @@
+package com.ontosim.model;
+
+public class OntoReferenceModel {
+
+	String srcKey;
+	String targetKey;
+	String srcNm;
+	String targetNm;
+	String srcAltNm;
+	String targetAltNm;
+	
+	
+	public String getSrcKey() {
+		return srcKey;
+	}
+	public void setSrcKey(String srcKey) {
+		this.srcKey = srcKey;
+	}
+	public String getTargetKey() {
+		return targetKey;
+	}
+	public void setTargetKey(String targetKey) {
+		this.targetKey = targetKey;
+	}
+	public String getSrcNm() {
+		return srcNm;
+	}
+	public void setSrcNm(String srcNm) {
+		this.srcNm = srcNm;
+	}
+	public String getTargetNm() {
+		return targetNm;
+	}
+	public void setTargetNm(String targetNm) {
+		this.targetNm = targetNm;
+	}
+	public String getSrcAltNm() {
+		return srcAltNm;
+	}
+	public void setSrcAltNm(String srcAltNm) {
+		this.srcAltNm = srcAltNm;
+	}
+	public String getTargetAltNm() {
+		return targetAltNm;
+	}
+	public void setTargetAltNm(String targetAltNm) {
+		this.targetAltNm = targetAltNm;
+	}
+	
+}
