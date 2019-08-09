4f8ee381c983054924e31e261ef911de2d688a72
jaydeepchakraborty <jaydeep.chakraborty.1988@gmail.com>
2019-08-09 19:53:02 +0530

    Added Constant file for Python

---
+++ linking_python/OntoSimPY/OntoSimConstants.py
@@ -0,0 +1,37 @@
+# colab_code_path = "/content/drive/My Drive/deep_learning/OntoSimilarity/"
+# code_path = colab_code_path
+# gcp_code_path = "/home/jaydeep_chakraborty_1988/research/OntoSimilarity/"
+# code_path = gcp_code_path
+
+# dev_code_path = "/Users/jaydeep/jaydeep_workstation/ASU/Research/oaei/2018/KnowledgeGraphs/LargeBio_dataset_oaei2019/" \
+#                   "local_Gen/task_1_NCI_FMA/"
+# code_path = dev_code_path
+
+prod_code_path = "/home/OntoSimPY/"
+code_path = prod_code_path
+
+
+# dev_faxt_text_model_path = "/Users/jaydeep/jaydeep_workstation/ASU/Research/oaei/fasttext/"
+# faxt_text_model_path = dev_faxt_text_model_path
+
+prod_faxt_text_model_path = "/app/model/fasttext/"
+faxt_text_model_path = prod_faxt_text_model_path
+
+
+# dev_onto_model_path = "/app/model/fasttext/"
+# onto_model_path = dev_onto_model_path
+
+
+prod_onto_model_path = "/app/"
+onto_model_path = prod_onto_model_path
+
+
+epoch_num = 10
+show_epoch_info = 1
+
+left_range = 0
+right_range = 1
+
+top_k = 5
+
+wait_time = 15
