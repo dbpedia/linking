d395b0755040f0a17002a6d7fc91225e659081a2
jaydeepchakraborty <jaydeep.chakraborty.1988@gmail.com>
2019-08-09 19:08:24 +0530

    Modified shell script for docker build

--- ontobuilt.sh
+++ ontobuilt.sh
@@ -37,17 +37,43 @@ fi
 
 if [ "$envnm" = $DEV ] && [ "$actnm" = $START ]
 then
-    docker-compose -f docker-compose-dev.yml up --build
+
+    echo "Do you want to build[Y/N] ? "
+    read build_ind
+    if [ "$build_ind" == "Y" ] || [ "$build_ind" == "y" ];
+    then
+        BUILD_NEEDED="True";
+    fi
+
+    if [ $BUILD_NEEDED = "True" ];
+    then
+        ./ontodrivecrt.sh dev
+        docker-compose -f docker-compose-dev.yml up --build
+    else
+        ./ontodrivecrt.sh dev
+        docker-compose -f docker-compose-dev.yml up --no-build
+    fi
+
+
 elif [ "$envnm" = $PROD ] && [ "$actnm" = $START ]
 then
-    if [[ $(docker images -q ontosimui:v1) = "" ]]; then  BUILD_NEEDED="True"; fi
-    if [[ $(docker images -q ontosimjava:v1) = "" ]]; then  BUILD_NEEDED="True"; fi
-    if [[ $(docker images -q ontosimpy:v1) == "" ]]; then BUILD_NEEDED="True"; fi
-	if [ $BUILD_NEEDED = "True" ]; then
+
+    echo "--> Do you want to build[Y/N] ? "
+    read build_ind
+    if [ "$build_ind" == "Y" ] || [ "$build_ind" == "y" ];
+    then
+        BUILD_NEEDED="True";
+    fi
+
+	if [ $BUILD_NEEDED = "True" ];
+    then
+        ./ontodrivecrt.sh prod
         docker-compose -f docker-compose-prod.yml up --build
     else
+        ./ontodrivecrt.sh prod
         docker-compose -f docker-compose-prod.yml up --no-build
-	fi
+    fi
+
 elif [  "$envnm" = $DEV ] && [ "$actnm" = $STOP ]
 then
     docker-compose -f docker-compose-dev.yml down
@@ -59,6 +85,9 @@ then
     docker rmi -f $(docker images -q ontosimui:v1)
     docker rmi -f $(docker images -q ontosimjava:v1)
     docker rmi -f $(docker images -q ontosimpy:v1)
+    docker rmi -f $(docker images -q ontosimdrive:v1)
+    docker rmi -f $(docker images -f "dangling=true" -q)
+    docker system prune
 else
-    echo "Environment or Activity is  not recognised"
+    echo "\033[31m ERROR: Environment or Activity is  not recognised \033[0m"
 fi
