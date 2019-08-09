308841726c8d61a20b223e908d74b1d0b02ea712
jaydeepchakraborty <jaydeep.chakraborty.1988@gmail.com>
2019-08-09 20:51:49 +0530

    Modified UI OntoFile pojo

--- linking_java/OntoSimilarity/src/main/java/com/ontosim/model/OntoFileMsg.java
+++ linking_java/OntoSimilarity/src/main/java/com/ontosim/model/OntoFileMsg.java
@@ -2,6 +2,7 @@ package com.ontosim.model;
 
 public class OntoFileMsg {
 	String msg_val;
+	String msg_cause;
 
 	public String getMsg_val() {
 		return msg_val;
@@ -9,5 +10,13 @@ public class OntoFileMsg {
 
 	public void setMsg_val(String msg_val) {
 		this.msg_val = msg_val;
+	}
+
+	public String getMsg_cause() {
+		return msg_cause;
+	}
+
+	public void setMsg_cause(String msg_cause) {
+		this.msg_cause = msg_cause;
 	}
 }
