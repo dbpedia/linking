075ffa88681d44adac6b6b1dd51f7be97080d788
jaydeepchakraborty <jaydeep.chakraborty.1988@gmail.com>
2019-08-09 20:30:05 +0530

    Removed page2 and page3 from UI routing

--- linking_ui/OntoSimUI/src/app/app-routing.module.ts
+++ linking_ui/OntoSimUI/src/app/app-routing.module.ts
@@ -2,13 +2,10 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 import { Pg1Component } from './pg1/pg1.component';
-import { Pg2Component } from './pg2/pg2.component';
-import { Pg3Component } from './pg3/pg3.component';
 
 const routes: Routes = [
+	{path: '', component: Pg1Component},
 	{path: 'pg1', component: Pg1Component},
-	{path: 'pg2', component: Pg2Component},
-	{path: 'pg3', component: Pg3Component}
 ];
 
 @NgModule({
@@ -17,4 +14,4 @@ const routes: Routes = [
 })
 
 export class AppRoutingModule { }
-export const routingComponents = [Pg1Component, Pg2Component, Pg3Component]
+export const routingComponents = [Pg1Component]
