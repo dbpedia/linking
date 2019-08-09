9c2f5260977bec3bb1110d0b4ad05369e1adaf10
jaydeepchakraborty <jaydeep.chakraborty.1988@gmail.com>
2019-08-09 20:54:41 +0530

    Modified Java Service Entrypoint

--- linking_java/OntoSimilarity/src/main/java/com/ontosim/service/OntoSimService.java
+++ linking_java/OntoSimilarity/src/main/java/com/ontosim/service/OntoSimService.java
@@ -7,8 +7,13 @@ import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
+import org.codehaus.plexus.util.ExceptionUtils;
+
 import com.ontosim.bl.OntoSimBL;
+import com.ontosim.cnst.OntoConst;
+import com.ontosim.exception.OntoException;
 import com.ontosim.model.OntoFileModel;
+import com.ontosim.model.OntoFileMsg;
 import com.ontosim.model.OntoServiceModel;
 import com.ontosim.util.OWLUtil;
 
@@ -22,39 +27,59 @@ public class OntoSimService {
 	@Consumes(MediaType.APPLICATION_JSON)
 	@Produces(MediaType.APPLICATION_JSON)
 	@Path("task1")
-	public String parseOntoOWL(String ontoInput) {
-
+	public String ontoJavaMtdh(String ontoInput) {
+		System.out.println("#################### Java OWL API START ####################");
 		String retJSONStr = "";
 		try {
-			if("".equals(ontoInput)){
-				return retJSONStr;
+			if ("".equals(ontoInput)) {
+				throw new OntoException(OntoConst.ERR_MSG_1);
 			}
-			
+
 			OntoServiceModel ontoServiceModel = owlUtil.convertToServiceModel(ontoInput);
 
-			String srcBase64_Fl = ontoSimBL.ontoSimBL(ontoServiceModel.getSrc_in_data());
-			OntoFileModel src_op_data = new OntoFileModel();
-			src_op_data.setFile(srcBase64_Fl);
-			src_op_data.setFile_nm("source.json");
-			src_op_data.setFile_typ("application/json");
-			ontoServiceModel.setSrc_op_data(src_op_data);
+			if (ontoServiceModel.getSrc_in_data().getFile() == null) {
+				throw new OntoException(OntoConst.ERR_MSG_2);
+			}
 
-			String trgtBase64_Fl = ontoSimBL.ontoSimBL(ontoServiceModel.getTrgt_in_data());
-			OntoFileModel trgt_op_data = new OntoFileModel();
-			trgt_op_data.setFile(trgtBase64_Fl);
-			trgt_op_data.setFile_nm("target.json");
-			trgt_op_data.setFile_typ("application/json");
-			ontoServiceModel.setTrgt_op_data(trgt_op_data);
+			if (ontoServiceModel.getTrgt_in_data().getFile() == null) {
+				throw new OntoException(OntoConst.ERR_MSG_3);
+			}
+
+			String srcBase64_Fl = ontoSimBL.ontoSimBL(ontoServiceModel.getSrc_in_data(), OntoConst.SRC);
+			OntoFileModel src_in_data = new OntoFileModel();
+			src_in_data.setFile(srcBase64_Fl);
+			src_in_data.setFile_nm(OntoConst.SRC_FL);
+			src_in_data.setFile_typ(OntoConst.FL_TYP);
+			ontoServiceModel.setSrc_in_data(src_in_data);
+
+			String trgtBase64_Fl = ontoSimBL.ontoSimBL(ontoServiceModel.getTrgt_in_data(), OntoConst.TRGT);
+			OntoFileModel trgt_in_data = new OntoFileModel();
+			trgt_in_data.setFile(trgtBase64_Fl);
+			trgt_in_data.setFile_nm(OntoConst.TRGT_FL);
+			trgt_in_data.setFile_typ(OntoConst.FL_TYP);
+			ontoServiceModel.setTrgt_in_data(trgt_in_data);
 
 			retJSONStr = owlUtil.convertFrmServiceModel(ontoServiceModel);
 
 		} catch (Exception e) {
-			e.printStackTrace();
+			OntoServiceModel ontoServiceModel = new OntoServiceModel();
+			OntoFileMsg ontoFileMsg = new OntoFileMsg();
+			ontoFileMsg.setMsg_val(OntoConst.ERR);
+			ontoFileMsg.setMsg_cause(ExceptionUtils.getFullStackTrace(e));
+			ontoServiceModel.setMsg(ontoFileMsg);
+			try {
+				retJSONStr = owlUtil.convertFrmServiceModel(ontoServiceModel);
+			} catch (Exception e1) {
+				// This should never happen, should be tested
+				e1.printStackTrace();
+			}
+		} finally {
+			System.out.println("#################### Java OWL API STOP ####################");
 		}
 
 		return retJSONStr;
 	}
-	
+
 	@GET
 	@Path("testtask")
 	public String testWebService() {
