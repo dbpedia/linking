1e7f8c7afb8e9ffcca09b07b9b0835eb0c892ce3
jaydeepchakraborty <jaydeep.chakraborty.1988@gmail.com>
2019-08-09 20:40:27 +0530

    Modified Java constant file

--- linking_java/OntoSimilarity/src/main/java/com/ontosim/cnst/OntoConst.java
+++ linking_java/OntoSimilarity/src/main/java/com/ontosim/cnst/OntoConst.java
@@ -15,5 +15,23 @@ public interface OntoConst {
 	String OntoMaxCardinality = "ObjectMaxCardinality";
 	String OntoExactCardinality = "ObjectExactCardinality";
 	String OntoHasValuef = "ObjectHasValue";
+	
+	String ERR = "Error Happened";
+	String ERR_MSG_1 = "Input is blank";
+	String ERR_MSG_2 = "Source file is missing";
+	String ERR_MSG_3 = "Target file is missing";
+	
+	String CLS_TYP = "Class";
+	String OBJ_TYP = "ObjectProp";
+	String DATA_TYP = "DataProp";
+	
+	String SRC = "source";
+	String TRGT = "target";
+	String SRC_FL = "source.json";
+	String TRGT_FL = "target.json";
+	String FL_EXT = ".json";
+	String FL_TYP = "application/json";
+	
+	String BASE_IND = "data:application/octet-stream;base64,";
 
 }
