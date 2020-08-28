a2662dbb8beebf22645349ed2b42ef15f95f6d56
Jaydeep <jaydeep.chakraborty.1988@gmail.com>
2020-08-27 21:08:59 -0700

    Add - Batch time

--- OntoSim/linking_java/OntoSimilarity/src/main/java/com/ontosim/adapter/OntoConnBatchMain.java
+++ OntoSim/linking_java/OntoSimilarity/src/main/java/com/ontosim/adapter/OntoConnBatchMain.java
@@ -2,6 +2,8 @@ package com.ontosim.adapter;
 
 import java.io.File;
 import java.text.SimpleDateFormat;
+import java.time.Duration;
+import java.time.Instant;
 import java.util.Date;
 
 import org.slf4j.Logger;
@@ -15,22 +17,36 @@ public class OntoConnBatchMain {
 	public static void main(String args[]) {
 
 		logger.info("#################### OntoSim Batch START ####################");
+		Instant start = Instant.now();
 		
 		try{
 			
 			String ontoFlPath = System.getProperty("user.dir") + File.separator + "ontofiles/local";
 			String srcFlPath = ontoFlPath + File.separator + "source.owl";
 			String trgtFlPath = ontoFlPath + File.separator + "target.owl";
-
+			
 			String currTimeStamp = new SimpleDateFormat("yyyy_MM_dd_HH_mm_ss").format(new Date());
 			String resultsPath = ontoFlPath + File.separator + "result_"+currTimeStamp+".rdf";
 			
 			// Calling OntoConnController
 			OntoConnController ontoConnController = new OntoConnController();
 			ontoConnController.ontoConnController(srcFlPath, trgtFlPath, resultsPath, "Anatomy");
-			
+						
 		}catch(Exception e){
 			logger.error("Exception occur:- ", e);
+		}finally{
+			Instant end = Instant.now();
+			Duration timeElapsed = Duration.between(start, end);
+			long ms = timeElapsed.toMillis();
+			System.out.println("Time taken: "+ ms +" milliseconds");
+			
+			
+			long diffSeconds = (ms / 1000) % 60;
+			long diffMinutes = (ms / (60 * 1000)) % 60;
+			long diffHours = (ms / (60 * 60 * 1000)) % 24;
+
+			System.out.println("Time taken: "+ diffHours +" hours, "+ diffMinutes +" minutes, "+ diffSeconds +" seconds.");
+
 		}
 
 		logger.info("#################### OntoSim Batch STOP ####################");
--- ontobuiltbatch.sh
+++ ontobuiltbatch.sh
@@ -7,7 +7,14 @@ buildind=$1
 
 echo -e "\033[93m ------> Please Enter Source & Target OWL file path \033[0m"
 read onto_dir
-echo -e "\033[93m ------> Output will be generated in the following path \033[0m"
+echo -e "\033[93m ------> Please Enter Source OWL file name \033[0m"
+read src_fl
+cp $onto_dir/$src_fl $onto_dir/"source.owl"
+echo -e "\033[93m ------> Please Enter Target OWL file name \033[0m"
+read trgt_fl
+cp $onto_dir/$trgt_fl $onto_dir/"target.owl"
+
+echo -e "\033[36m Output-rdf (with Timestamp) will be generated in the following path:- \033[0m"
 echo $onto_dir
 
 if [ $buildind = "True" ];
@@ -16,5 +23,7 @@ then
     docker run --name ontosim_app_batch -v $onto_dir:/usr/ontosim/java/ontofiles/local/ ontosim_imgbatch:v1
 elif [ $buildind = "False" ];
 then
+    docker stop ontosim_app_batch
+    docker rm ontosim_app_batch
     docker run --name ontosim_app_batch -v $onto_dir:/usr/ontosim/java/ontofiles/local/ ontosim_imgbatch:v1
 fi
