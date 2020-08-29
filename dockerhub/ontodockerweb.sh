5bbdb52a5757a50e6afc04633f25d17835116104
Jaydeep <jaydeep.chakraborty.1988@gmail.com>
2020-08-29 08:29:02 -0700

    Update - read me

--- dockerhub/ontodockerweb.sh
+++ dockerhub/ontodockerweb.sh
@@ -11,3 +11,20 @@ then
 else
     echo -e "\033[31;3m Activity is ${actind} \033[0m"
 fi
+
+if [ "$actind" = "start" ];
+then
+    docker stop ontosim_app_web
+    docker rm ontosim_app_web
+    docker pull jchakra1/ontosim_imgweb:v1
+    docker run --name ontosim_app_web -p 8080:8080 jchakra1/ontosim_imgweb:v1
+elif [ "$actind" = "stop" ];
+then
+    docker stop ontosim_app_web
+    docker rm ontosim_app_web
+elif [ "$actind" = "del" ];
+then
+    docker stop ontosim_app_web
+    docker rm ontosim_app_web
+    docker rmi -f $(docker images -q jchakra1/ontosim_imgweb:v1)
+fi
